dc066361d28fc5378ec7bf5b835046d0d4e79a08
OliveiraPedro09 <pedromartinsoliveira9@gmail.com>
2025-09-08 16:51:21 -0300

    feat: docker compose initial commit

--- src/main/java/com/codenine/managementservice/ManagementServiceApplication.java
+++ src/main/java/com/codenine/managementservice/ManagementServiceApplication.java
@@ -9,5 +9,4 @@ public class ManagementServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementServiceApplication.class, args);
 	}
-
 }
--- src/main/java/com/codenine/managementservice/entity/Item.java
+++ src/main/java/com/codenine/managementservice/entity/Item.java
@@ -1,6 +1,10 @@
 package com.codenine.managementservice.entity;
 
 import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.Instant;
