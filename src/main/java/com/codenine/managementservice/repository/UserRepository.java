fea6e94b772ee07ca00ae25e37aee35b4dd312e3
Davi Maciel Ferreira <104574671+DfMaciel@users.noreply.github.com>
2025-09-19 10:40:34 -0300

    feat: Listagem de usuários adicionada

--- src/main/java/com/codenine/managementservice/repository/UserRepository.java
+++ src/main/java/com/codenine/managementservice/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.codenine.managementservice.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import com.codenine.managementservice.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
+  List<User> findBySections_IdIn(List<Long> sectionIds);
 }
--- src/main/java/com/codenine/managementservice/service/UserService.java
+++ src/main/java/com/codenine/managementservice/service/UserService.java
@@ -1,26 +1,55 @@
 package com.codenine.managementservice.service;
 
+import com.codenine.managementservice.dto.user.Role;
+import com.codenine.managementservice.dto.user.UserRequest;
+import com.codenine.managementservice.entity.Section;
+import com.codenine.managementservice.entity.User;
+import com.codenine.managementservice.repository.SectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.codenine.managementservice.repository.UserRepository;
 
-import jakarta.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class UserService {
 
-  @Autowired private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-  @Transactional
-  public void printUserSections(String email) {
-    userRepository
-        .findByEmail(email)
-        .ifPresent(
-            user -> {
-              System.out.println("User: " + user.getUsername());
-              user.getSections()
-                  .forEach(section -> System.out.println("Section: " + section.getTitle()));
-            });
+  @Autowired
+  private SectionRepository sectionRepository;
+
+  public void createUser(UserRequest userRequest) {
+        List<Section> sections = getSectionsByIds(userRequest.sectionIds());
+        User user = new User();
+        user.setName(userRequest.name());
+        user.setEmail(userRequest.email());
+        user.setPassword(userRequest.password());
+        user.setRole(userRequest.role());
+        user.setSections(sections);
+        userRepository.save(user);
+  }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new NullPointerException("User not found with id: " + id));
+    }
+
+    public List<User> getAllUsers(Authentication authentication) {
+      User user = (User) authentication.getPrincipal();
+        if (user.getRole().equals(Role.MANAGER)) {
+            List<Long> sectionIds = user.getSections().stream()
+                .map(Section::getId).toList();
+            List<User> users = userRepository.findBySections_IdIn(sectionIds);
+            return users;
+        }
+        return userRepository.findAll();
+    }
+
+  private List<Section> getSectionsByIds(List<Long> sectionIds) {
+      return sectionRepository.findAllById(sectionIds);
   }
 }
