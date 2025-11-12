8191c1057a71cdd4701f5c5165074154f8d26808
Davi Maciel Ferreira <104574671+DfMaciel@users.noreply.github.com>
2025-11-12 10:37:38 -0300

    feat: Atualização das autenticações de seção, ordem de compra e notificação

--- src/main/java/com/codenine/managementservice/controller/PurchaseOrderController.java
+++ src/main/java/com/codenine/managementservice/controller/PurchaseOrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -37,6 +38,7 @@ public class PurchaseOrderController {
   public final EmailService emailService;
 
   @PostMapping("/")
+  @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
   public ResponseEntity<?> createPurchaseOrder(
       @RequestBody PurchaseOrderRequest request, Authorization authorization) {
     try {
